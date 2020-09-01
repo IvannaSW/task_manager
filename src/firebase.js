d878115b041455bb9dd04bb4ca41c8a614091c44
IvannaSW <ivanna.sav08@gmail.com>
2020-09-01 21:38:49 +0300

    Connecr firebase and test gettig of data from FireStore

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyAsKrD1LmWuqqUnmg9hKotA5iW-WHJaZDI",
+  authDomain: "task-manager-45733.firebaseapp.com",
+  databaseURL: "https://task-manager-45733.firebaseio.com",
+  projectId: "task-manager-45733",
+  storageBucket: "task-manager-45733.appspot.com",
+  messagingSenderId: "293606961371",
+  appId: "1:293606961371:web:bb27c710aa6decdadadcfe",
+};
+
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+const db = firebase.firestore();;
+
+export {db};
